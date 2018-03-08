0b9f33820461da95c96fa0260d49c4e0946270d7
Patryk Milewski <mpatryk97@gmail.com>
2018-03-08 14:08:21 +0100

    Init!

---
+++ UwB_UI/Data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UwB_UI
+{
+	public class Table
+	{
+		public Table(params (int key, List<DateTime> times)[] records) =>
+			this.Records = records.ToDictionary(i => i.key, i => i.times);
+
+		public Table(Dictionary<int, List<DateTime>> records) => 
+			this.Records = records;
+
+		public override string ToString() =>
+			String.Join("\n", this.Records.Select(i => String.Join("\n", i.Value.Select(j => $"{i.Key} {j}"))));
+
+		public List<DateTime> this[int key] => this.Records[key];
+
+		public Dictionary<int, List<DateTime>> Records;
+	}
+}
